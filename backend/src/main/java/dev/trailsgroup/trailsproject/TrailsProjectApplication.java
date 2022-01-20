7ad32eab4c7672de5c4fbd6a9d50b5375280b733
Emanuel Martins <emanuelmartins284@gmail.com>
2022-01-20 08:58:03 -0300

    Backend created

---
+++ backend/src/main/java/dev/trailsgroup/trailsproject/TrailsProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.trailsgroup.trailsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrailsProjectApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/dev/trailsgroup/trailsproject/TrailsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.trailsgroup.trailsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
