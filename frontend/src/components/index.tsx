ed5a2fc5899424b6f73747e546250ea5539b8b91
= <=>
2022-01-21 09:07:55 -0300

    created: NavBar component

---
+++ frontend/src/components/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { FiLogIn } from "react-icons/fi";
+
+import "./styles.scss";
+import { ReactComponent as Logo } from "../assets/images/Logo.svg";
+
+export function NavBar() {
+  return (
+    <nav className="navbar">
+      <Logo />
+      <div>
+        <Link to="#">Trilhas</Link>
+        <Link to="#">Sobre</Link>
+        <Link to="#">Cadastre-se</Link>
+        <button>
+          <FiLogIn color="var(--white)" size={18} />
+          Entrar
+        </button>
+      </div>
+    </nav>
+  );
+}
