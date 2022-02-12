83488b7827892f0f8c9c995a847061a0e13ef028
Emanuel Martins <emanuelmartins284@gmail.com>
2022-02-12 08:30:08 -0300

    create: Subject entity

---
+++ backend/src/main/java/dev/trailsgroup/trailsproject/entities/Subject.java
@@ -0,0 +1,95 @@
+package dev.trailsgroup.trailsproject.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_subject")
+public class Subject implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String image;
+    private String grade;
+    private String htmlContent;
+
+
+    @ManyToOne
+    private Topic topic;
+
+    public Subject(){}
+
+    public Subject(Integer id, String name, String image, String grade, String htmlContent, Topic topic) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.grade = grade;
+        this.htmlContent = htmlContent;
+        this.topic = topic;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getHtmlContent() {
+        return htmlContent;
+    }
+
+    public void setHtmlContent(String htmlContent) {
+        this.htmlContent = htmlContent;
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Subject subject = (Subject) o;
+        return id.equals(subject.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
