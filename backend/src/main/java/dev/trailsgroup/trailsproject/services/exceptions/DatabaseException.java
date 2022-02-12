f31965ddfc754572c2e6dd01a78158fb9bbf1f13
Emanuel Martins <emanuelmartins284@gmail.com>
2022-02-12 10:08:27 -0300

    created: Exceptions

---
+++ backend/src/main/java/dev/trailsgroup/trailsproject/services/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package dev.trailsgroup.trailsproject.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+    public DatabaseException(String msg){
+        super(msg);
+    }
+}
