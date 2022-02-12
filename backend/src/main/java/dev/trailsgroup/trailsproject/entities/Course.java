c23c86044e5e4d9472dd8c7feca39413cd7708bf
Emanuel Martins <emanuelmartins284@gmail.com>
2022-02-12 10:08:08 -0300

    nothing

--- backend/src/main/java/dev/trailsgroup/trailsproject/entities/Course.java
+++ backend/src/main/java/dev/trailsgroup/trailsproject/entities/Course.java
@@ -20,6 +20,7 @@ public class Course implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
     private String image;
 
