c879f34a93744d555f31d7a51b20e1967e874748
= <=>
2022-02-12 07:27:47 -0300

    created: SideNav component

---
+++ frontend/src/components/SideNav/index.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { FiMenu } from 'react-icons/fi';
+import './styles.scss';
+
+export function SideNav(){
+
+    const [isVisible, setIsVisible] = useState(false)
+
+    function toggleSidebar(){
+        setIsVisible(!isVisible);
+    }
+
+    return (
+        <FiMenu onClick={toggleSidebar} className="hamburguer" color="var(--green)" size={24} />
+    )
+}
