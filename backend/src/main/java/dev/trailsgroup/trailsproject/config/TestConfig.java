e3c49c686ee396c75310f179b9f22f45c81f9ad2
Emanuel Martins <emanuelmartins284@gmail.com>
2022-02-12 11:43:10 -0300

    create: TestConfig file

---
+++ backend/src/main/java/dev/trailsgroup/trailsproject/config/TestConfig.java
@@ -0,0 +1,26 @@
+crpackage dev.trailsgroup.trailsproject.config;
+
+import dev.trailsgroup.trailsproject.entities.User;
+import dev.trailsgroup.trailsproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        User u1 = new User(null, "Emanuel", "123", "emanuelmartins@gmail.com", 1, true);
+        userRepository.save(u1);
+
+
+    }
+
+}
