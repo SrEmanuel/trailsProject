95259ab517a440939a8767e4e0f6b7006bd8cc26
--replace <edmarcosfilho2015@gmail.com>
2022-02-12 09:03:04 -0300

    refactor: index.tsx and styles.scss moved into Home

--- frontend/src/pages/Landing/index.tsx
+++ frontend/src/pages/Landing/index.tsx
@@ -1,9 +1,10 @@
-import { NavBar } from "../components/Navbar";
+import { NavBar } from "../../components/Navbar";
 
 export function Home(){
     return(
         <div className="container">
             <NavBar />
+            
         </div>
     )
 }
