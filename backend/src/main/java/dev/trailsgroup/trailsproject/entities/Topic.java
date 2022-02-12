508601b9541158eb9dc9ba3e527542e2b579a917
Emanuel Martins <emanuelmartins284@gmail.com>
2022-02-12 08:30:33 -0300

    create: Topic entity

---
+++ backend/src/main/java/dev/trailsgroup/trailsproject/entities/Topic.java
@@ -0,0 +1,82 @@
+package dev.trailsgroup.trailsproject.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_topic")
+public class Topic implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+
+    private Integer position; //variable to show in witch position this topic really is
+
+
+    @ManyToOne
+    private Course course;
+
+    @OneToMany
+    private Set<Subject> subjects;
+
+    public Topic(Integer id, String name, Integer position, Course course) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.course = course;
+    }
+
+    public Topic() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Topic topic = (Topic) o;
+        return id.equals(topic.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
