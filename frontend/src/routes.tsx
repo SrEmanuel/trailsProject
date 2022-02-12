be4cf8dcddaaa096862ed9c6ed1a2264f4a6cf60
--replace <edmarcosfilho2015@gmail.com>
2022-02-12 19:02:13 -0300

    update: added route for Login component

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes as RoutesContainer } from 'react-router-dom';
 import { Home } from './pages/Landing';
+import { Login } from './pages/Login';
 
 export function Routes(){
     return(
         <BrowserRouter>
             <RoutesContainer>
              <Route path="/" element={<Home></Home>}  />
+             <Route path="/login" element={<Login></Login>} />
             </RoutesContainer>
         </BrowserRouter>
     )
