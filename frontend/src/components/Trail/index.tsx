b60ec88cd6b3aff82455b502498d23c8d201952f
--replace <edmarcosfilho2015@gmail.com>
2022-02-12 19:49:38 -0300

    create: Trail component

---
+++ frontend/src/components/Trail/index.tsx
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+export function Trail(){
+    return(
+        <div className="card-container">
+            <div>
+                Matemática
+            </div>
+        </div>
+    )
+}
