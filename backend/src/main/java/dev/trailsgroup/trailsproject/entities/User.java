a9ec150afde941e94d3339974154b7321de4235b
Emanuel Martins <emanuelmartins284@gmail.com>
2022-02-12 08:31:47 -0300

    create: User entity

---
+++ backend/src/main/java/dev/trailsgroup/trailsproject/entities/User.java
@@ -0,0 +1,93 @@
+package dev.trailsgroup.trailsproject.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String password;
+    private String email;
+    private Integer type;
+    private Boolean status;
+    //TODO implements createdAt and uptadedAt, if necessary
+
+    public User(){}
+
+    public User(Integer id, String name, String password, String email, Integer type, Boolean status) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.type = type;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id) && email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email);
+    }
+}
