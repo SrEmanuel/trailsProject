186e67f9e3689bef09339946cf0b6533addb35fe
--replace <edmarcosfilho2015@gmail.com>
2022-02-13 15:12:40 -0300

    update: added card styles and content

--- frontend/src/components/Trail/index.tsx
+++ frontend/src/components/Trail/index.tsx
@@ -1,11 +1,25 @@
-import './styles.scss';
+import "./styles.scss";
 
-export function Trail(){
-    return(
-        <div className="card-container">
-            <div>
-                Matemática
-            </div>
-        </div>
-    )
+import exampleImage from "../../assets/images/subject.png";
+import { FiFileText } from "react-icons/fi";
+
+export function Trail() {
+  return (
+    <div className="card-container">
+      <div className="card-header">
+        <img src={exampleImage} alt="capa do card" />
+        <span>Matemática</span>
+      </div>
+      <div className="card-bottom">
+        <span>
+          <FiFileText color="var(--purple)" size={24} />
+          34 - artigos
+        </span>
+        <span>
+          <FiFileText color="var(--red)" size={24} />
+          34 - exercícios
+        </span>
+      </div>
+    </div>
+  );
 }
