a9508daff5a395a81259e840b8fa13d678ef2147
--replace <edmarcosfilho2015@gmail.com>
2022-02-14 09:02:12 -0300

    update: rendering Subjects instead of Trails if the route is diferent

--- frontend/src/pages/ListContent/index.tsx
+++ frontend/src/pages/ListContent/index.tsx
@@ -1,19 +1,24 @@
+import { useLocation } from "react-router-dom";
 import { NavBar } from "../../components/Navbar";
 import { Paginator } from "../../components/Paginator";
+import { Subject } from "../../components/Subject";
 import { Trail } from "../../components/Trail";
 import "./styles.scss";
 
 export function ListContent() {
   const trails = [1, 2, 3, 4 , 5, 6 , 7, 8, 9, 10, 11, 12];
-
+  const subjects = [1, 2, 3, 4 , 5, 6 , 7, 8, 9, 10, 11, 12];
+  const location = useLocation()
   return (
     <div className="container">
       <NavBar />
       <h1>Trilhas disponíveis</h1>
       <Paginator />
       <div className="trails-grid-container">
-        { trails.map((trail, index ) =>(
-            <Trail color={index % 3 === 0? 'var(--green)' : index % 3 === 1? 'var(--red)' : 'vaR(--purple)' } />
+        { location.pathname==="/cursos"?  trails.map((trail, index ) =>(
+            <Trail color={index % 3 === 0? 'var(--green)' : index % 3 === 1? 'var(--red)' : 'var(--purple)' } />
+        )) : subjects.map(subject =>(
+          <Subject />
         )) }
       </div>
     </div>
