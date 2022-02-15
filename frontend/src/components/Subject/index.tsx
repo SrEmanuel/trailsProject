b306d37ddf297cc00e8337afbe508352b1c1cc3d
--replace <edmarcosfilho2015@gmail.com>
2022-02-14 21:45:13 -0300

    update: added subject prop with data to display

--- frontend/src/components/Subject/index.tsx
+++ frontend/src/components/Subject/index.tsx
@@ -1,17 +1,22 @@
 import "./styles.scss";
 import example from '../../assets/images/subject-example.png';
 import { Link } from "react-router-dom";
+import { ISubject } from "../../interfaces/subject";
 
-export function Subject() {
+interface Props{
+  subject: ISubject;
+}
+
+export function Subject( {subject}: Props) {
   return (
     <div className="card-container subject">
       <div className="card-header">
         <img src={example} alt="capa do card" />
-        <Link to="#" >1º ano</Link>
+        <Link to="#" >{subject.grade}</Link>
       </div>
       <div className="card-line"></div>
       <div className="subject-card-bottom">
-        <span>1-Grécia antiga</span>
+        <span>{subject.position}-{subject.name}</span>
       </div>
     </div>
   );
