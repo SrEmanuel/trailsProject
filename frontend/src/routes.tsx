6b0dd19e132330bca10cb46d4b2aae68508b10ca
--replace <edmarcosfilho2015@gmail.com>
2022-02-24 08:48:05 -0300

    update: added route for register

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,6 +3,7 @@ import { BlogPost } from './pages/BlogPost';
 import { Home } from './pages/Landing';
 import { ListContent } from './pages/ListContent';
 import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 
 export function Routes(){
     return(
@@ -10,6 +11,7 @@ export function Routes(){
             <RoutesContainer>
              <Route path="/" element={<Home></Home>}  />
              <Route path="/login" element={<Login></Login>} />
+             <Route path="/cadastro" element={<Register></Register>} />
              <Route path="/cursos" element={<ListContent></ListContent>} />
              <Route path="/cursos/:courseid" element={<ListContent></ListContent>} />
              <Route path="/cursos/:courseid/:blogid" element={<BlogPost></BlogPost>} />
