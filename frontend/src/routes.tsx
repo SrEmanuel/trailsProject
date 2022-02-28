bc6ea4557f33e5bceeb5b24a5cdd1b7fd5c60b37
--replace <edmarcosfilho2015@gmail.com>
2022-02-28 15:34:42 -0300

    update: added route to ForgotPassword page

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes as RoutesContainer } from 'react-router-dom';
 import { About } from './pages/About';
 import { BlogPost } from './pages/BlogPost';
+import { ForgotPassword } from './pages/ForgotPassword';
 import { Home } from './pages/Landing';
 import { ListContent } from './pages/ListContent';
 import { Login } from './pages/Login';
@@ -14,6 +15,7 @@ export function Routes(){
              <Route path="/login" element={<Login></Login>} />
              <Route path="/cadastro" element={<Register></Register>} />
              <Route path="/sobre" element={<About></About>} />
+             <Route path="/esqueci-minha-senha" element={<ForgotPassword></ForgotPassword>} />
              <Route path="/cursos" element={<ListContent></ListContent>} />
              <Route path="/cursos/:courseid" element={<ListContent></ListContent>} />
              <Route path="/cursos/:courseid/:blogid" element={<BlogPost></BlogPost>} />
