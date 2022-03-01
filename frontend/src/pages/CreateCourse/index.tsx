b951675df9f7fe25e37f3285d99af7a5a6fa3a6f
--replace <edmarcosfilho2015@gmail.com>
2022-03-01 09:24:21 -0300

    create: CreateCourse page

---
+++ frontend/src/pages/CreateCourse/index.tsx
@@ -0,0 +1,28 @@
+import { FiArrowLeft } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+import { WaveContainer } from "../../components/WaveContainer";
+import "./styles.scss";
+
+export function CreateCourse() {
+
+  const navigate = useNavigate();
+
+  return (
+    <WaveContainer>
+      <span className="goBack" onClick={()=> navigate(-1)}>
+        <FiArrowLeft size={24} color="var(--dark-green)" /> Voltar
+      </span>
+      <main className="create-course-container">
+        <h1>Criar novo curso</h1>
+        <form>
+          <input type="file" name="dropzone" />
+          <input type="text" name="name" placeholder="Nome do curso" />
+          <div className="buttons-container">
+            <button>Cancelar</button>
+            <button>Avançar</button>
+          </div>
+        </form>
+      </main>
+    </WaveContainer>
+  );
+}
