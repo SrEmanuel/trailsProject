42a65686aa68c6deb942eea8f88bb908989895f9
Emanuel Martins <emanuelmartins284@gmail.com>
2022-03-03 10:34:28 -0300

    created: StaticFileService to deal with the saving of static files in disk

--- backend/src/main/java/dev/trailsgroup/trailsproject/services/StaticFileService.java
+++ backend/src/main/java/dev/trailsgroup/trailsproject/services/StaticFileService.java
@@ -11,6 +11,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+
 @Service
 public class StaticFileService {
 
@@ -32,22 +36,49 @@ public class StaticFileService {
         return ip;
     }
 
+    private String getPath(){
+        Path currentPath = Paths.get(".");
+        Path absolutePath = currentPath.toAbsolutePath();
+        return absolutePath + "/src/main/resources/static/uploads/";
+    }
+
     public String save(MultipartFile file){
         try {
-            Path currentPath = Paths.get(".");
-            Path absolutePath = currentPath.toAbsolutePath();
-            String path = absolutePath + "/src/main/resources/static/uploads/";
-            byte[] bytes = file.getBytes();
-            String fileName =  file.getOriginalFilename();
-            Path finalPath = Paths.get(path + fileName);
-            Files.write(finalPath, bytes);
 
-            return fileName;
+            String path = getPath();
+            byte[] bytes = file.getBytes();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd'T'HHmmssS");
+
+            String fileName = file.getOriginalFilename();
+            assert fileName != null;
+            String replacedFileName = fileName.split("\\.")[0].replaceAll("[^a-zA-Z0-9_.-]+", "-");
+
+            fileName = replacedFileName + "." + fileName.substring(fileName.lastIndexOf(".") + 1);
+            String newFileName = (sdf.format(new Date()) + "-" + fileName);
+
+            Path finalPath = Paths.get(path + newFileName);
+            Files.write(finalPath, bytes);
+            return newFileName;
         }catch(IOException e){
             e.printStackTrace();
-            throw new UploadException("a");
+            throw new UploadException("Houve um problema durante o envio do arquivo");
+        }catch(AssertionError e){
+            throw new UploadException("O nome do arquivo é nulo");
         }
     }
+
+    public String update(MultipartFile file, String oldName){
+        String path = getPath();
+        try {
+            Files.deleteIfExists(Path.of(path + oldName));
+            return save(file);
+        } catch (IOException e) {
+            throw new UploadException("Houve um erro durante a atualização do arquivo");
+        }
+    }
+
+
+
 
 
 
