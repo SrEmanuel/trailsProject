874bbef7e47f5a4fcb10d6aef8dc9662de550fd6
Emanuel Martins <emanuelmartins284@gmail.com>
2022-03-07 16:07:40 -0300

    create: implementation of findByEmail method

--- backend/src/main/java/dev/trailsgroup/trailsproject/repositories/UserRepository.java
+++ backend/src/main/java/dev/trailsgroup/trailsproject/repositories/UserRepository.java
@@ -2,6 +2,10 @@ package dev.trailsgroup.trailsproject.repositories;
 
 import dev.trailsgroup.trailsproject.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Transactional(readOnly = true)
+    User findByEmail(String email);
 }
