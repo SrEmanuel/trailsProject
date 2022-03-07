a531d3e25abb7f80f5ddfcf220fb6d54dd4345ed
Emanuel Martins <emanuelmartins284@gmail.com>
2022-03-07 11:01:02 -0300

    update: making users endpoint public to posts and gets

--- backend/src/main/java/dev/trailsgroup/trailsproject/config/SecurityConfig.java
+++ backend/src/main/java/dev/trailsgroup/trailsproject/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -26,8 +27,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/courses/**",
             "/topics/**",
             "/subjects/**",
+    };
+
+    private static final String[] PUBLIC_MATCHERS_POST = {
+            "/users/",
 
     };
+
 
     private static final String[] PUBLIC_MATCHERS = {
             "/h2-console/**"
@@ -43,6 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable();
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -53,5 +60,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
         return source;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
