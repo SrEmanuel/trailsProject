7ba2a39f13ed895100a993ae3d37e32aee031768
Emanuel Martins <emanuelmartins284@gmail.com>
2022-03-08 13:58:02 -0300

    update: adding findByEmail method into UserService

--- backend/src/main/java/dev/trailsgroup/trailsproject/services/UserService.java
+++ backend/src/main/java/dev/trailsgroup/trailsproject/services/UserService.java
@@ -44,6 +44,10 @@ public class UserService {
         return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
+    public User findByEmail(String email){
+        return repository.findByEmail(email);
+    }
+
     public User insert(UserDTO obj){
         try {
             if(!verifyEmailAvailability(obj.getEmail()))
