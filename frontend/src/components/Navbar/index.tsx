c6dbda6bdab0f85befb002ba57a40f899d89270b
--replace <edmarcosfilho2015@gmail.com>
2022-03-12 11:27:31 -0300

    update: showing log-in button if there is a loggedUser and a log-out button if not

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,25 +1,47 @@
-import { Link, useNavigate } from "react-router-dom";
-import { FiLogIn } from "react-icons/fi";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { FiLogIn, FiLogOut } from "react-icons/fi";
 
-import  Logo  from "../../assets/images/Logo.svg";
+import Logo from "../../assets/images/Logo.svg";
 import { SideMenu } from "../SideMenu";
 import "./styles.scss";
+import { useAuth } from "../../hooks/useAuth";
+import { useEffect, useState } from "react";
 
 export function NavBar() {
-
+  const { handleClearUserDataFromStorage, getUser } = useAuth();
+  const [hasLoggedUser, setHasLoggedUser] = useState<boolean>(false);
   const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const user = getUser();
+    user ? setHasLoggedUser(true) : setHasLoggedUser(false);
+  }, [location, getUser]);
 
   return (
     <nav className="navbar">
-      <img src={Logo} onClick={ ()=> navigate('/') } alt="Projeto Trilhas" />
+      <img src={Logo} onClick={() => navigate("/")} alt="Projeto Trilhas" />
       <div>
         <Link to="/cursos">Trilhas</Link>
         <Link to="/sobre">Sobre</Link>
-        <Link to="/cadastro">Cadastre-se</Link>
-        <Link className="login-btn" to ="/login">
-          <FiLogIn color="var(--white)" size={18} />
-          Entrar
-        </Link>
+        {!hasLoggedUser ? (
+          <>
+            <Link to="/cadastro">Cadastre-se</Link>
+            <Link className="login-btn" to="/login">
+              <FiLogIn color="var(--white)" size={18} />
+              Entrar
+            </Link>
+          </>
+        ) : (
+          <Link
+            onClick={() => handleClearUserDataFromStorage()}
+            className="logout-btn"
+            to="/"
+          >
+            <FiLogOut color="var(--white)" size={18} />
+            Sair
+          </Link>
+        )}
         <SideMenu />
       </div>
     </nav>
