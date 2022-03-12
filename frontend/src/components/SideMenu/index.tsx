7fad2df30bfe641d686e6a6ccf2d56c877d0f97e
--replace <edmarcosfilho2015@gmail.com>
2022-03-12 14:21:13 -0300

    update: receiving hasLoggedUser via props and showing a login button if true or a log-out button if false

--- frontend/src/components/SideMenu/index.tsx
+++ frontend/src/components/SideMenu/index.tsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
-import { FiMenu, FiBookOpen, FiInfo, FiLogIn } from "react-icons/fi";
+import { FiMenu, FiBookOpen, FiInfo, FiLogIn, FiLogOut } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 import "./styles.scss";
 
-export function SideMenu() {
+interface Props {
+  hasLoggedUser: boolean;
+}
+
+export function SideMenu({ hasLoggedUser }: Props) {
   const [isVisible, setIsVisible] = useState(false);
+  const { handleClearUserDataFromStorage } = useAuth();
 
   function toggleSidebar() {
     setIsVisible(!isVisible);
@@ -28,14 +34,27 @@ export function SideMenu() {
           <FiInfo color="var(--green)" size={24} />
           <span>Sobre</span>
         </Link>
-        <Link to="/cadastro">
-          <FiLogIn color="var(--green)" size={24} />
-          <span>Cadastre-se</span>
-        </Link>
-        <Link className="login-btn" to="/login" >
-          <FiLogIn color="var(--white)" size={18} />
-          Entrar
-        </Link>
+        {!hasLoggedUser ? (
+          <>
+            <Link to="/cadastro">
+              <FiLogIn color="var(--green)" size={24} />
+              <span>Cadastre-se</span>
+            </Link>
+            <Link className="login-btn" to="/login">
+              <FiLogIn color="var(--white)" size={18} />
+              Entrar
+            </Link>
+          </>
+        ) : (
+          <Link
+            onClick={() => handleClearUserDataFromStorage()}
+            className="logout-btn"
+            to="/"
+          >
+            <FiLogOut color="var(--white)" size={18} />
+            Sair
+          </Link>
+        )}
       </aside>
     </>
   );
