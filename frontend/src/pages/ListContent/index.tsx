1958a1070a2a60c948a1d767a30e2ed50d6624cf
--replace <edmarcosfilho2015@gmail.com>
2022-03-12 08:41:53 -0300

    fix: removed dynamic backgroudn colors and added keys to items inside a map

--- frontend/src/pages/ListContent/index.tsx
+++ frontend/src/pages/ListContent/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation,  useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { NavBar } from "../../components/Navbar";
 import { Paginator } from "../../components/Paginator";
 import { Subject } from "../../components/Subject";
@@ -12,7 +12,7 @@ import "./styles.scss";
 export function ListContent() {
   const [trails, setTrails] = useState<ITrails[]>();
   const [topics, setTopics] = useState<ITopic[]>();
-  const [courseName, setCourseName] = useState('');
+  const [courseName, setCourseName] = useState("");
 
   const location = useLocation();
   const params = useParams();
@@ -23,55 +23,55 @@ export function ListContent() {
       setTrails(response.data.content);
     }
 
-    async function handleLoadTopics(){
+    async function handleLoadTopics() {
       const response = await api.get(`/courses/${params.courseid}/topics`);
       setTopics(response.data);
     }
 
-    async function handleLoadSelectedCourse(){
+    async function handleLoadSelectedCourse() {
       const response = await api.get(`/courses/${params.courseid}/`);
       setCourseName(response.data.name);
     }
 
-    location.pathname ==='/cursos' ? handleLoadCourses() : handleLoadTopics()
-    handleLoadSelectedCourse()
-
+    if (location.pathname === "/cursos") {
+      handleLoadCourses();
+    } else {
+      handleLoadTopics();
+      handleLoadSelectedCourse();
+    }
   }, [location, params]);
 
   return (
     <div className="container">
       <NavBar />
       <h1>
-        {location.pathname === "/cursos"
-          ? "Trilhas disponíveis"
-          : courseName}
+        {location.pathname === "/cursos" ? "Trilhas disponíveis" : courseName}
       </h1>
       {location.pathname === "/cursos" && <Paginator />}
       <div className="trails-grid-container">
-        {location.pathname === "/cursos" && trails?.map((trail, index) => (
-              <Trail
+        {location.pathname === "/cursos" &&
+          trails?.map((trail, index) => (
+            <Trail
+              key={trail.id}
               trail={trail}
-                color={
-                  index % 3 === 0
-                    ? "var(--green)"
-                    : index % 3 === 1
-                    ? "var(--red)"
-                    : "var(--purple)"
-                }
-              />
-            ))
-          }
+            />
+          ))}
       </div>
-      { location.pathname !== '/cursos' && topics?.map((topic) => (
-            <>
+      {location.pathname !== "/cursos" &&
+        topics?.map((topic) => (
+          <>
             <h2 className="topic-title">{topic.name}</h2>
             <div className="trails-grid-container">
-            {topic.subjects.map((subject) =>(
-                <Subject courseId={params.courseid as string } subject={subject} />
-            ))}
+              {topic.subjects.map((subject) => (
+                <Subject
+                  key={subject.id}
+                  courseId={params.courseid as string}
+                  subject={subject}
+                />
+              ))}
             </div>
-            </>
-          ))}
+          </>
+        ))}
     </div>
   );
 }
