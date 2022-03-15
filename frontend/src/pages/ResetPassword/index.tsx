699e33a147f4f03bf77bea48e2f2fe382515058e
--replace <edmarcosfilho2015@gmail.com>
2022-03-14 21:43:48 -0300

    update: added Formik validation, toast notification and handleSubmitNewPassword method

--- frontend/src/pages/ResetPassword/index.tsx
+++ frontend/src/pages/ResetPassword/index.tsx
@@ -1,14 +1,34 @@
 import { WaveContainer } from "../../components/WaveContainer";
 import logo from "../../assets/images/Logo.svg";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { ForestContainer } from "../../components/ForestContainer/indext";
+import { Formik } from "formik";
+import { ResetPasswordSchema } from "../../schemas/resetPasswordSchema";
+import { toast, ToastContainer } from "react-toastify";
+import api from "../../services/api";
 
 export function ResetPassword() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  async function handleSubmitNewPassword(values: {
+    password: string;
+    confirmPassword: string;
+  }) {
+    const token = location.search;
+    console.log(token);
+    try {
+      const response = await api.post(`/auth/change-password${token}`, values);
+      toast.success(response.data.message);
+    } catch (error: any) {
+      toast.error(error.data.message);
+    }
+  }
 
   return (
     <WaveContainer>
       <ForestContainer>
+        <ToastContainer />
         <main className="password-container">
           <img onClick={() => navigate("/")} src={logo} alt="Projeto trilhas" />
           <h1>Insira a sua nova senha</h1>
@@ -17,11 +37,41 @@ export function ResetPassword() {
             lembrar
           </span>
 
-          <form>
-            <input type="password" name="password" placeholder="Nova senha" />
-            <input type="password" name="confirmPassword" placeholder="Confirmar senha" />
-            <button>Enviar</button>
-          </form>
+          <Formik
+            initialValues={{ password: "", confirmPassword: "" }}
+            validationSchema={ResetPasswordSchema}
+            onSubmit={(values) => handleSubmitNewPassword(values)}
+          >
+            {({ handleChange, handleSubmit, errors, touched }) => (
+              <form>
+                <input
+                  onChange={handleChange}
+                  type="password"
+                  name="password"
+                  placeholder="Nova senha"
+                />
+
+                {errors.password && touched.password && (
+                  <span className="error text">{errors.password}</span>
+                )}
+
+                <input
+                  onChange={handleChange}
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Confirmar senha"
+                />
+
+                {errors.confirmPassword && touched.confirmPassword && (
+                  <span className="error text">{errors.confirmPassword}</span>
+                )}
+
+                <button type="button" onClick={() => handleSubmit()}>
+                  Enviar
+                </button>
+              </form>
+            )}
+          </Formik>
         </main>
       </ForestContainer>
     </WaveContainer>
