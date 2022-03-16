8871a285af07db12f2c1c177750092e890b92e23
Emanuel Martins <emanuelmartins284@gmail.com>
2022-03-16 10:29:13 -0300

    update: sending html personalized emails with javaMailSender

--- backend/src/main/java/dev/trailsgroup/trailsproject/services/AbstractEmailService.java
+++ backend/src/main/java/dev/trailsgroup/trailsproject/services/AbstractEmailService.java
@@ -1,9 +1,17 @@
 package dev.trailsgroup.trailsproject.services;
 
 import dev.trailsgroup.trailsproject.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.exceptions.TemplateInputException;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.Date;
 
 public abstract class AbstractEmailService implements EmailService{
@@ -13,20 +21,51 @@ public abstract class AbstractEmailService implements EmailService{
     @Value("${default.mail.sender}")
     private String sender;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     @Override
     public void sendNewTokenEmail(User user, String token){
-        SimpleMailMessage sm = prepareNewPasswordEmail(user, token);
-        sendEmail(sm);
+        try {
+            MimeMessage mm = prepareNewPasswordEmailHTML(user, token);
+            sendHtmlEmail(mm);
+        }catch (MessagingException | TemplateInputException e ){
+            SimpleMailMessage sm = prepareNewPasswordEmail(user, token);
+            sendEmail(sm);
+        }
     }
 
     protected SimpleMailMessage prepareNewPasswordEmail(User user, String token) {
         SimpleMailMessage sm = new SimpleMailMessage();
         sm.setTo(user.getEmail());
         sm.setFrom("Projeto Trilhas <"+ sender +">");
-        sm.setSubject("Link de recuperação da sua senha!");
+        sm.setSubject("Link de recuperação de senha!");
         sm.setSentDate(new Date(System.currentTimeMillis()));
-        sm.setText("Aqui está o seu token: " + ip +"/nova-senha?token="+ token);
+        sm.setText("Aqui está o seu token: " + ip +"/nova-senha?token=" + token);
         return sm;
     }
+
+    protected MimeMessage prepareNewPasswordEmailHTML(User user, String token) throws MessagingException{
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
+        mmh.setTo(user.getEmail());
+        mmh.setFrom("Projeto Trilhas <"+ sender +">");
+        mmh.setSubject("Link de recuperação de senha!");
+        mmh.setSentDate(new Date(System.currentTimeMillis()));
+        mmh.setText(prepareHTML(user, token), true);
+        return mimeMessage;
+    }
+
+    protected String prepareHTML(User user, String token){
+        Context context = new Context();
+        context.setVariable("user", user);
+        context.setVariable("token", ip +"/nova-senha?token="+token);
+        return templateEngine.process("email/passwordEmail", context);
+
+    }
+
+
 }
--- backend/src/main/java/dev/trailsgroup/trailsproject/services/SmtpEmailService.java
+++ backend/src/main/java/dev/trailsgroup/trailsproject/services/SmtpEmailService.java
@@ -1,15 +1,22 @@
 package dev.trailsgroup.trailsproject.services;
 
+import dev.trailsgroup.trailsproject.entities.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.mail.internet.MimeMessage;
 
 public class SmtpEmailService extends AbstractEmailService{
 
     @Autowired
     private MailSender mailSender;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 
@@ -18,5 +25,10 @@ public class SmtpEmailService extends AbstractEmailService{
         LOG.info("Simulando envio de email!");
         mailSender.send(msg);
         LOG.info("Email enviado!");
+    }
+
+    @Override
+    public void sendHtmlEmail(MimeMessage msg) {
+        javaMailSender.send(msg);
     }
 }
