1148fc755d3a1ac430a7c915cb888cfce597bd82
--replace <edmarcosfilho2015@gmail.com>
2022-03-16 22:26:22 -0300

    update: added showOptions prop to control when will the edit and delete options be available to the user

--- frontend/src/components/Subject/index.tsx
+++ frontend/src/components/Subject/index.tsx
@@ -1,25 +1,37 @@
 import "./styles.scss";
 import { Link, useNavigate } from "react-router-dom";
 import { ISubject } from "../../interfaces/subject";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
 
-interface Props{
+interface Props {
   subject: ISubject;
   courseId: string;
+  showOptions: boolean;
 }
 
-export function Subject( {subject, courseId}: Props) {
-
+export function Subject({ subject, courseId, showOptions }: Props) {
   const navigate = useNavigate();
 
   return (
-    <div onClick={()=> navigate(`/cursos/${courseId}/${subject.id}`) } className="card-container subject">
+    <div
+      onClick={() => navigate(`/cursos/${courseId}/${subject.id}`)}
+      className="card-container subject"
+    >
       <div className="card-header">
         <img src={subject.imagePath} alt="capa do card" />
-        <Link to="#" >{subject.grade}</Link>
+        <Link to="#">{subject.grade}</Link>
       </div>
       <div className="card-line"></div>
       <div className="subject-card-bottom">
-        <span>{subject.position}-{subject.name}</span>
+        <span>
+          {subject.position}-{subject.name}
+        </span>
+        {showOptions && (
+          <div>
+            <FiEdit color="var(--purple)" size={24} />
+            <FiTrash2 color="var(--red)" size={24} />
+          </div>
+        )}
       </div>
     </div>
   );
