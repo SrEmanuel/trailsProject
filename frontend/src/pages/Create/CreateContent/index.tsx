2a17ac2fce45ee59019fab3b000f9f07d37fb9da
--replace <edmarcosfilho2015@gmail.com>
2022-03-26 15:23:09 -0300

    update: added inputHandler function that triggers with ckeditor's onChange event and uses formikref to set the htmlContent data into formik state

--- frontend/src/pages/Create/CreateContent/index.tsx
+++ frontend/src/pages/Create/CreateContent/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { FiArrowLeft } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 import { CKEditor } from "ckeditor4-react";
@@ -6,9 +6,9 @@ import { Dropzone } from "../../../components/Dropzone";
 import { WaveContainer } from "../../../components/WaveContainer";
 import "../styles.scss";
 import "./styles.scss";
-import { Formik } from "formik";
+import { Formik} from "formik";
 import { NewContentSchema } from "../../../schemas/newcontent.schema";
-import { HtmlContentSchema } from "../../../schemas/htmlcontent.schema";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 interface PostData {
   title: string;
@@ -20,9 +20,14 @@ interface PostData {
 export function CreateContent() {
   const [step, setStep] = useState(1);
   const navigate = useNavigate();
+  const formikRef = useRef() as any;
 
   function handleSubmitStepOne(values: PostData) {
     setStep(2);
+    console.log(values);
+  }
+  function inputHandler(event: any, editor: any) {
+    formikRef?.current?.setFieldValue("htmlContent", event.editor.getData());
   }
 
   return (
@@ -33,7 +38,8 @@ export function CreateContent() {
       <main className="create-container">
         <h1>Criar nova postagem</h1>
         <Formik
-          initialValues={{ grade: "", title: "", image: "" }}
+          innerRef={formikRef}
+          initialValues={{ grade: "", title: "", image: "", htmlContent: "" }}
           validationSchema={NewContentSchema}
           onSubmit={(values) => handleSubmitStepOne(values)}
         >
@@ -73,18 +79,26 @@ export function CreateContent() {
                 </>
               ) : (
                 <CKEditor
+                  editor={ClassicEditor}
                   style={{ width: "90%" }}
                   initData={
                     <p>
-                      Este é o seu editor. Use-o para criar seus textos e postagens!
+                      Este é o seu editor. Use-o para criar seus textos e
+                      postagens!
                     </p>
                   }
+                  onChange={inputHandler as any}
                 />
               )}
               <div className="buttons-container">
-                <button type="button" onClick={() => step===1? navigate(-1) : setStep(1) } >{step===1? 'Cancelar': 'Voltar'}</button>
+                <button
+                  type="button"
+                  onClick={() => (step === 1 ? navigate(-1) : setStep(1))}
+                >
+                  {step === 1 ? "Cancelar" : "Voltar"}
+                </button>
                 <button type="button" onClick={() => handleSubmit()}>
-                {step===1? 'Avançar': 'Terminar'}
+                  {step === 1 ? "Avançar" : "Terminar"}
                 </button>
               </div>
             </form>
