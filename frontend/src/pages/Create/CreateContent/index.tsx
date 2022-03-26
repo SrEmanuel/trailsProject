c7688046e2705b3716cac704f4a1fe88e9a6fb61
--replace <edmarcosfilho2015@gmail.com>
2022-03-26 13:36:28 -0300

    refactor: moved all the inputs inside the Formik element and displaying diferent messages with diferrent click on the buttons depending of the current form step

--- frontend/src/pages/Create/CreateContent/index.tsx
+++ frontend/src/pages/Create/CreateContent/index.tsx
@@ -8,14 +8,20 @@ import "../styles.scss";
 import "./styles.scss";
 import { Formik } from "formik";
 import { NewContentSchema } from "../../../schemas/newcontent.schema";
+import { HtmlContentSchema } from "../../../schemas/htmlcontent.schema";
+
+interface PostData {
+  title: string;
+  grade: string;
+  image: string;
+  htmlContent?: string;
+}
 
 export function CreateContent() {
   const [step, setStep] = useState(1);
-  const [stepOneData, setStepOneData] = useState();
   const navigate = useNavigate();
 
-  function handleSubmitStepOne(values: any) {
-    setStepOneData(values);
+  function handleSubmitStepOne(values: PostData) {
     setStep(2);
   }
 
@@ -26,60 +32,64 @@ export function CreateContent() {
       </span>
       <main className="create-container">
         <h1>Criar nova postagem</h1>
-        {step === 1 ? (
-          <Formik
-            initialValues={{ grade: "", title: "", image:"" }}
-            validationSchema={NewContentSchema}
-            onSubmit={(values) => handleSubmitStepOne(values)}
-          >
-            {({ handleSubmit, handleChange, errors, touched }) => (
-              <form>
-                <Dropzone onChange={handleChange} />
-                {errors.image && touched.image && <span className="error text">{errors.image}</span>}
-                <input
-                  onChange={handleChange}
-                  type="text"
-                  name="title"
-                  placeholder="Título"
-                />
-                {errors.title && touched.title && <span className="error text">{errors.title}</span>}
+        <Formik
+          initialValues={{ grade: "", title: "", image: "" }}
+          validationSchema={NewContentSchema}
+          onSubmit={(values) => handleSubmitStepOne(values)}
+        >
+          {({ handleSubmit, handleChange, errors, touched }) => (
+            <form>
+              {step === 1 ? (
+                <>
+                  <Dropzone onChange={handleChange} />
+                  {errors.image && touched.image && (
+                    <span className="error text">{errors.image}</span>
+                  )}
+                  <input
+                    onChange={handleChange}
+                    type="text"
+                    name="title"
+                    placeholder="Título"
+                  />
+                  {errors.title && touched.title && (
+                    <span className="error text">{errors.title}</span>
+                  )}
 
-                <select
-                  onChange={handleChange}
-                  name="grade"
-                  placeholder="Selecione uma série"
-                >
-                  <option selected disabled hidden value="0">
-                    Selecione uma opção
-                  </option>
-                  <option value="1">1º ano</option>
-                  <option value="2">2º ano</option>
-                  <option value="3">3º ano</option>
-                </select>
-                {errors.grade && touched.grade && <span className="error text">{errors.grade}</span>}
-                <div className="buttons-container">
-                  <button>Cancelar</button>
-                  <button type="button" onClick={() => handleSubmit()}>
-                    Avançar
-                  </button>
-                </div>
-              </form>
-            )}
-          </Formik>
-        ) : (
-          <>
-            <CKEditor
-              style={{ width: "90%" }}
-              initData={
-                <p>This is an example CKEditor 4 WYSIWYG editor instance.</p>
-              }
-            />
-            <div className="buttons-container">
-              <button>Voltar</button>
-              <button onClick={() => setStep(2)}>Concluir</button>
-            </div>
-          </>
-        )}
+                  <select
+                    onChange={handleChange}
+                    name="grade"
+                    placeholder="Selecione uma série"
+                  >
+                    <option selected disabled hidden value="0">
+                      Selecione uma opção
+                    </option>
+                    <option value="1">1º ano</option>
+                    <option value="2">2º ano</option>
+                    <option value="3">3º ano</option>
+                  </select>
+                  {errors.grade && touched.grade && (
+                    <span className="error text">{errors.grade}</span>
+                  )}
+                </>
+              ) : (
+                <CKEditor
+                  style={{ width: "90%" }}
+                  initData={
+                    <p>
+                      Este é o seu editor. Use-o para criar seus textos e postagens!
+                    </p>
+                  }
+                />
+              )}
+              <div className="buttons-container">
+                <button type="button" onClick={() => step===1? navigate(-1) : setStep(1) } >{step===1? 'Cancelar': 'Voltar'}</button>
+                <button type="button" onClick={() => handleSubmit()}>
+                {step===1? 'Avançar': 'Terminar'}
+                </button>
+              </div>
+            </form>
+          )}
+        </Formik>
       </main>
     </WaveContainer>
   );
