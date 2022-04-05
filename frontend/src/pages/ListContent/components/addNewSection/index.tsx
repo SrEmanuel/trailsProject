2bdaf6efa5b4ab19e09186b94a058d64ed7f6846
--replace <edmarcosfilho2015@gmail.com>
2022-04-05 09:51:51 -0300

    update: added html content

--- frontend/src/pages/ListContent/components/addNewSection/index.tsx
+++ frontend/src/pages/ListContent/components/addNewSection/index.tsx
@@ -1,4 +1,9 @@
+import { FiX } from 'react-icons/fi';
+
+import { ModalContainer } from "../../../../components/ModalContainer";
 import { Overlay } from "../../../../components/Overlay";
+
+import './styles.scss';
 
 interface Props{
   isVisible: boolean;
@@ -7,7 +12,15 @@ interface Props{
 export function AddNewSection({isVisible}: Props){
   return(
     <Overlay hidden={!isVisible} >
-      the content
+      <ModalContainer>
+        <FiX size={24} color="var(--red)" />
+        <h2>Deseja adicionar uma nova sessão?</h2>
+        <input placeholder='Nome da sessão' />
+        <div className="btn-container">
+          <button>Cancelar</button>
+          <button>Criar</button>
+        </div>
+      </ModalContainer>
     </Overlay>
   )
 }
