298ff36f5c609cd9b83a5b528352300ba87c82ef
--replace <edmarcosfilho2015@gmail.com>
2022-04-05 10:35:33 -0300

    update: added setSection prop that updates the newSection value

--- frontend/src/pages/ListContent/components/addNewSection/index.tsx
+++ frontend/src/pages/ListContent/components/addNewSection/index.tsx
@@ -1,26 +1,27 @@
-import { FiX } from 'react-icons/fi';
+import { FiX } from "react-icons/fi";
 
 import { ModalContainer } from "../../../../components/ModalContainer";
 import { Overlay } from "../../../../components/Overlay";
 
-import './styles.scss';
+import "./styles.scss";
 
-interface Props{
+interface Props {
   isVisible: boolean;
+  setSection: React.Dispatch<React.SetStateAction<string | undefined>>;
 }
 
-export function AddNewSection({isVisible}: Props){
-  return(
-    <Overlay hidden={!isVisible} >
+export function AddNewSection({ isVisible, setSection }: Props) {
+  return (
+    <Overlay hidden={!isVisible}>
       <ModalContainer>
         <FiX size={24} color="var(--red)" />
         <h2>Deseja adicionar uma nova sessão?</h2>
-        <input placeholder='Nome da sessão' />
+        <input onChange={ (e) => setSection(e.target.value)} placeholder="Nome da sessão" />
         <div className="btn-container">
           <button>Cancelar</button>
           <button>Criar</button>
         </div>
       </ModalContainer>
     </Overlay>
-  )
+  );
 }
