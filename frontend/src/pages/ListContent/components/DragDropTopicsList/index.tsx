a878f11b8346093c928d1ef3c21537dba069c3bf
--replace <edmarcosfilho2015@gmail.com>
2022-05-21 12:51:03 -0300

    update:  created separated onDrag function to handle the topics upload logic and added a updateList function to send the changes to the api

--- frontend/src/pages/ListContent/components/DragDropTopicsList/index.tsx
+++ frontend/src/pages/ListContent/components/DragDropTopicsList/index.tsx
@@ -1,26 +1,29 @@
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
+import { useNavigate } from "react-router-dom";
 import { Topic } from "../../../../components/Topic";
+import { useAuth } from "../../../../hooks/useAuth";
 import { ISubject } from "../../../../interfaces/subject";
 import { ITopic } from "../../../../interfaces/topic";
+import api from "../../../../services/api";
+import { handleNotifyError } from "../../../../utils/handleNotifyError";
 
 interface Props {
   topics: ITopic[];
   params: any;
-  onContentChange: () => void;
+  onContentChange: () => void | Promise<void>;
   setTopics: React.Dispatch<React.SetStateAction<ITopic[] | undefined>>;
 }
 
-export function DragDropTopicsList({
-  topics,
-  setTopics,
-  params,
-  onContentChange,
-}: Props) {
-  const reorder = (
-    subjects: ISubject[],
-    startIndex: number,
-    endIndex: number
-  ) => {
+interface UpdatePositionPayload {
+  subjectId: string | number;
+  position: number;
+}
+
+export function DragDropTopicsList({ params, topics, onContentChange, setTopics}: Props) {
+  const navigate = useNavigate();
+  const { handleClearUserDataFromStorage } = useAuth();
+
+  const reorder = (subjects: ISubject[], startIndex: number, endIndex: number) => {
     const result = Array.from(subjects);
     const [removed] = result.splice(startIndex, 1);
     result.splice(endIndex, 0, removed);
@@ -28,29 +31,48 @@ export function DragDropTopicsList({
     return result;
   };
 
+  const updateList = async(linkName: string, data: UpdatePositionPayload[]) => {
+    try {
+      await api.post(`/topics/${linkName}/update-positions`, data)
+      await onContentChange();
+    } catch (error) {
+      handleNotifyError(error, navigate, handleClearUserDataFromStorage);
+    }
+  }
+
+  const onDragEnd = (result: DropResult) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const index = Number(result.source.droppableId);
+
+    const selectedTopic = topics[index];
+
+    const reorderedSubjects = reorder(
+      selectedTopic.subjects,
+      result.source.index,
+      result.destination.index
+    );
+
+    const newTopic = { ...selectedTopic, subjects: reorderedSubjects };
+
+    topics.splice(index, 1, newTopic);
+
+    const data: UpdatePositionPayload[] = [];
+
+    topics.forEach( topic => {
+      topic.subjects.forEach( (subject, index) => data.push( { subjectId: subject.id, position: index } ) )
+    })
+
+    setTopics(topics);
+
+    updateList(selectedTopic.linkName, data);
+  }
+
   return (
     <DragDropContext
-      onDragEnd={(result) => {
-        if (!result.destination) {
-          return;
-        }
-
-        const index = Number(result.source.droppableId);
-
-        const selectedTopic = topics[index];
-
-        const reorderedSubjects = reorder(
-          selectedTopic.subjects,
-          result.source.index,
-          result.destination.index
-        );
-
-        const newTopic = { ...selectedTopic, subjects: reorderedSubjects };
-
-        topics.splice(index, 1, newTopic);
-
-        setTopics(topics);
-      }}
+      onDragEnd={onDragEnd}
     >
       {topics?.map((topic, index) => (
         <Droppable
