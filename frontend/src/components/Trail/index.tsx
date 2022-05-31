34b362582e67f937c912d7b274dd41221bae64b6
--replace <edmarcosfilho2015@gmail.com>
2022-05-31 10:09:17 -0300

    update: calculating percentage of progress in the course and dynamically setting the width of the progress bar

--- frontend/src/components/Trail/index.tsx
+++ frontend/src/components/Trail/index.tsx
@@ -6,11 +6,15 @@ import { useNavigate } from "react-router-dom";
 interface Props {
   color?: string;
   trail: ITrails;
-  mode: "teacher" | 'student' | "guest" | "admin";
+  mode: "teacher" | "student" | "guest" | "admin";
 }
 
 export function Trail({ color, trail, mode }: Props) {
   const navigate = useNavigate();
+
+  const percent: number = trail.completedCount
+    ? (trail.completedCount * 100) / trail.subjectsCount
+    : 0;
 
   return (
     <div
@@ -23,7 +27,7 @@ export function Trail({ color, trail, mode }: Props) {
       <div className="trail-bottom">
         <div className="title-and-options">
           <h3>{trail.name}</h3>
-          {(mode === 'admin' || mode ==='teacher') && (
+          {(mode === "admin" || mode === "teacher") && (
             <div className="options">
               <FiEdit size={20} color="var(--grey)" />
               <FiTrash2 size={20} color="var(--grey)" />
@@ -34,9 +38,9 @@ export function Trail({ color, trail, mode }: Props) {
         {mode === "student" && (
           <>
             <div className="progress-bar">
-              <div className="current-progress"></div>
+              <div style={{ width: `${percent}%` }} className="current-progress"></div>
             </div>
-            <span className="progress-label">20% concluido</span>
+            <span className="progress-label">{percent}% concluido</span>
           </>
         )}
       </div>
