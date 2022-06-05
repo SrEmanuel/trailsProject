2f27c816968acb56944f0127359bfcfddc493a85
--replace <edmarcosfilho2015@gmail.com>
2022-06-05 14:44:00 -0300

    create: UpdateCourseSchema

--- frontend/src/schemas/newcourse.schema.ts
+++ frontend/src/schemas/newcourse.schema.ts
@@ -5,3 +5,9 @@ export const NewCourseSchema = Yup.object().shape({
  professorID: Yup.string().required('O professor é um campo obrigatório'),
  image:Yup.mixed().required('A imagem é um campo obrigatório')
 });
+
+export const UpdateCourseSchema = Yup.object().shape({
+  name: Yup.string().required('O titulo é um campo obrigatório'),
+  professorID: Yup.string(),
+  image:Yup.mixed().required('A imagem é um campo obrigatório')
+})
