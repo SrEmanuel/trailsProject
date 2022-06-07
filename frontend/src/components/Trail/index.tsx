8b71d17d00ef7c57142a21693303c4fddc696283
--replace <edmarcosfilho2015@gmail.com>
2022-06-07 16:24:16 -0300

    update: added handleShowModal to ask if the user wants to delete the course, handleDeleteCourse to call the endpoint that deletes the course and onChange prop that executes a generic function at the end

--- frontend/src/components/Trail/index.tsx
+++ frontend/src/components/Trail/index.tsx
@@ -2,15 +2,23 @@ import "./styles.scss";
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 import { ITrails } from "../../interfaces/Trail";
 import { useNavigate } from "react-router-dom";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
+import { ConfirmationModal } from "../ConfirmationModal";
+import api from "../../services/api";
+import { toast } from "react-toastify";
+import { handleNotifyError } from "../../utils/handleNotifyError";
+import { useAuth } from "../../hooks/useAuth";
 
 interface Props {
   color?: string;
   trail: ITrails;
   mode: "teacher" | "student" | "guest" | "admin";
+  onChange?: (...args: any) => void;
 }
 
-export function Trail({ color, trail, mode }: Props) {
+export function Trail({ color, trail, mode, onChange }: Props) {
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+  const { handleClearUserDataFromStorage } = useAuth();
   const navigate = useNavigate();
 
   const percent: number = trail.completedCount
@@ -22,17 +30,46 @@ export function Trail({ color, trail, mode }: Props) {
     navigate(`/cursos/atualizar/${trail.linkName}`);
   }
 
+  function handleShowModal(e: FormEvent) {
+    setIsDeleteModalVisible(true);
+  }
+
+  async function handleDeleteCourse() {
+    try {
+      await api.delete(`/courses/${trail.linkName}`);
+      toast.success("Deletado com sucesso");
+      setIsDeleteModalVisible(false);
+      onChange && onChange();
+    } catch (error) {
+      if ((error as any).data.status === 400) {
+        toast.error(
+          "Não é possível excluir esse curso, pois ele não está vazio"
+        );
+      } else {
+        handleNotifyError(error, navigate, handleClearUserDataFromStorage);
+      }
+    }
+  }
+
   return (
-    <div
-      onClick={() => navigate(`/cursos/${trail.linkName}`)}
-      className="trail-container"
-      style={{ background: color }}
-    >
-      <img src={trail.imagePath} alt="capa do card" />
+    <div className="trail-container" style={{ background: color }}>
+      <ConfirmationModal
+        message="Deseja realmente excluir esse conteúdo?"
+        confirmText="Deletar"
+        cancelText="cancelar"
+        isVisible={isDeleteModalVisible}
+        setIsVisible={setIsDeleteModalVisible}
+        onConfirm={handleDeleteCourse}
+      />
+      <img
+        onClick={() => navigate(`/cursos/${trail.linkName}`)}
+        src={trail.imagePath}
+        alt="capa do card"
+      />
       <div className="card-line"></div>
       <div
         className={`trail-bottom ${
-          (mode !== 'student' ) && "smaller-card-bottom"
+          mode !== "student" && "smaller-card-bottom"
         }`}
       >
         <div className="title-and-options">
@@ -44,7 +81,11 @@ export function Trail({ color, trail, mode }: Props) {
                 size={20}
                 color="var(--grey)"
               />
-              <FiTrash2 size={20} color="var(--grey)" />
+              <FiTrash2
+                onClick={handleShowModal}
+                size={20}
+                color="var(--grey)"
+              />
             </div>
           )}
           {mode === "teacher" && (
