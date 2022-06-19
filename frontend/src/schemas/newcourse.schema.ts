d06e8487eb8e782c8efdf0e339e4e4191c56b68d
--replace <edmarcosfilho2015@gmail.com>
2022-06-19 13:10:38 -0300

    update: replaced professorID string by professors array with at least one element required to be valid

--- frontend/src/schemas/newcourse.schema.ts
+++ frontend/src/schemas/newcourse.schema.ts
@@ -2,12 +2,12 @@ import * as Yup from "yup";
 
 export const NewCourseSchema = Yup.object().shape({
  name: Yup.string().required('O titulo é um campo obrigatório'),
- professorID: Yup.string().required('O professor é um campo obrigatório'),
+ rofessors: Yup.array().min(1, 'É necessário ao menos um professor').required('É necessário ao menos um professor'),
  image:Yup.mixed().required('A imagem é um campo obrigatório')
 });
 
 export const UpdateCourseSchema = Yup.object().shape({
   name: Yup.string().required('O titulo é um campo obrigatório'),
-  professorID: Yup.string(),
+  professors: Yup.array().min(1, 'É necessário ao menos um professor').required('É necessário ao menos um professor'),
   image:Yup.mixed().required('A imagem é um campo obrigatório')
 })
