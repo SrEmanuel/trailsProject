d7d698904659d08d88f9bd952d4e55f7cf3f1c76
--replace <edmarcosfilho2015@gmail.com>
2022-06-19 18:13:01 -0300

    update: added span to display the current value, input for editing the value, folowing the isEdittingEnabled state,  and a new type prop for some imputs

--- frontend/src/components/Editable/index.tsx
+++ frontend/src/components/Editable/index.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+import { FiEdit, FiSave } from "react-icons/fi";
 import "./styles.scss";
 
 interface Props {
   label: string;
   value: string;
   onSubmit: (value: string) => void;
+  type?: string;
 }
 
-export function Editable({ label, value, onSubmit }: Props) {
-  return <div>edit</div>;
+export function Editable({ label, value, onSubmit, type }: Props) {
+  const [isEdittingEnabled, setIsEditingEnabled] = useState<boolean>(false);
+  return (
+   <div className="editable" >
+    <span className="label" >{label}</span>
+    { isEdittingEnabled? (
+      <div className="row-wrapper">
+        <input type={ type? type: "text" } value={value} />
+        <FiSave color="var(--grey)" size={20} onClick={()=> setIsEditingEnabled(false) } />
+      </div>
+    ) : (
+     <div className="row-wrapper">
+       <span className="value" >{value}</span>
+       <FiEdit color="var(--grey)" size={20} onClick={() => setIsEditingEnabled(true)} />
+     </div>
+    ) }
+   </div>
+  )
 }
