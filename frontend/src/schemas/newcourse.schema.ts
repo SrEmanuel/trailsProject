241bac9ec0de953aae32ab4c818a392e8ceeacc3
--replace <edmarcosfilho2015@gmail.com>
2022-06-19 13:32:39 -0300

    fix: spell error on "professors" prop

--- frontend/src/schemas/newcourse.schema.ts
+++ frontend/src/schemas/newcourse.schema.ts
@@ -2,7 +2,7 @@ import * as Yup from "yup";
 
 export const NewCourseSchema = Yup.object().shape({
  name: Yup.string().required('O titulo é um campo obrigatório'),
- rofessors: Yup.array().min(1, 'É necessário ao menos um professor').required('É necessário ao menos um professor'),
+ professors: Yup.array().min(1, 'É necessário ao menos um professor').required('É necessário ao menos um professor'),
  image:Yup.mixed().required('A imagem é um campo obrigatório')
 });
 
