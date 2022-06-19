27c4c48ac82f17d323d5dae5ee8006b262744492
--replace <edmarcosfilho2015@gmail.com>
2022-06-19 13:09:28 -0300

    update: added MUI chip, stack and avatar componets to create a chip list for each user selected on the select element to be a professor and added handleAddTeacher and handleRemoveTeacher methods. replaced professorId unique prop by professors array in the initialValues

--- frontend/src/pages/Create/CreateCourse/index.tsx
+++ frontend/src/pages/Create/CreateCourse/index.tsx
@@ -1,8 +1,11 @@
 import { Formik } from "formik";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react";
 import { FiArrowLeft } from "react-icons/fi";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import Avatar from "@mui/material/Avatar";
 import { Dropzone } from "../../../components/Dropzone";
 import { WaveContainer } from "../../../components/WaveContainer";
 import { useAuth } from "../../../hooks/useAuth";
@@ -16,7 +19,7 @@ import { handleNotifyError } from "../../../utils/handleNotifyError";
 import "../styles.scss";
 
 interface ITeacher {
-  id: number;
+  id: string;
   name: string;
   email: string;
 }
@@ -24,13 +27,14 @@ interface ITeacher {
 interface ICourse {
   image: string;
   name: string;
-  professorID?: number | string ;
+  professors?: ITeacher[];
 }
 
 export function CreateCourse() {
   const { getIsAdmin, handleClearUserDataFromStorage, user } = useAuth();
   const [initialValues, setInitialValues] = useState<ICourse>();
   const [teachers, setTeachers] = useState<ITeacher[]>();
+  const [selectedTeachers, setSelectedTeachers] = useState<ITeacher[]>([]);
   const params = useParams();
   const navigate = useNavigate();
   const location = useLocation();
@@ -38,6 +42,24 @@ export function CreateCourse() {
 
   function imgHandler(event: any) {
     formikRef.current.setFieldValue("image", event.target.files[0]);
+  }
+
+  function handleAddTeacher(e: ChangeEvent<HTMLSelectElement>) {
+    const teacher =
+      teachers && teachers[(e.target.value as unknown as number) - 1];
+    formikRef.current.setFieldValue("professors", [
+      ...selectedTeachers,
+      teacher,
+    ]);
+    teacher && setSelectedTeachers([...selectedTeachers, teacher]);
+  }
+
+  function handleRemoveTeacher(id: string) {
+    formikRef.current.setFieldValue(
+      "professors",
+      selectedTeachers.filter((t) => t.id !== id)
+    );
+    setSelectedTeachers(selectedTeachers.filter((t) => t.id !== id));
   }
 
   const handleLoadUsers = useCallback(async () => {
@@ -62,7 +84,7 @@ export function CreateCourse() {
   const handleCreateCourse = async (course: ICourse) => {
     const formData = new FormData();
     formData.append("image", course.image);
-    const courseData = { name: course.name, professorID: course.professorID };
+    const courseData = { name: course.name, professors: course.professors };
 
     try {
       const response = location.pathname.includes("atualizar")
@@ -70,7 +92,7 @@ export function CreateCourse() {
         : await api.post("/courses", courseData);
 
       await api.post(`/courses/${response.data.linkName}/add-image`, formData);
-      toast.success("Curso criado com sucesso");
+      toast.success("Enviado com sucesso!");
       navigate(-1);
     } catch (error) {
       handleNotifyError(error, navigate, handleClearUserDataFromStorage);
@@ -91,7 +113,14 @@ export function CreateCourse() {
 
     if (location.pathname.includes("atualizar")) {
       handleLoadCurrentCourseData().then((course) => {
-        setInitialValues({ name: course.name, image: course.imagePath, professorID: course.professors[0]?.id || 0 });
+        setInitialValues({
+          name: course.name,
+          image: course.imagePath,
+          professors: course.professors,
+        });
+
+        setSelectedTeachers(course.professors);
+
         fetch(course.imagePath).then(async (response) => {
           const blob = await response.blob();
           const file = new File([blob], course.imagePath.split("/uploads/")[1]);
@@ -102,7 +131,7 @@ export function CreateCourse() {
       setInitialValues({
         name: "",
         image: "",
-        professorID: 0,
+        professors: [],
       });
     }
   }, [
@@ -155,24 +184,39 @@ export function CreateCourse() {
                   <span className="error text">{errors.name}</span>
                 )}
                 {user?.roles.includes("ADMIN") && (
-                  <select
-                    name="professorID"
-                    placeholder="Selecione uma série"
-                    onChange={handleChange}
-                    value={values.professorID}
-                  >
-                    <option disabled hidden value="0">
-                      Selecione o professor da turma
-                    </option>
-                    {teachers?.map((teacher) => (
-                      <option key={teacher.id} value={teacher.id}>
-                        {teacher.name}
+                  <>
+                    <select onChange={handleAddTeacher} value={0}>
+                      <option disabled hidden value="0">
+                        Adicionar professor...
                       </option>
-                    ))}
-                  </select>
+                      {teachers?.map((teacher) => (
+                        <option key={teacher.id} value={teacher.id}>
+                          {teacher.name}
+                        </option>
+                      ))}
+                    </select>
+                    <Stack className="chip-stack" direction="row" spacing={1}>
+                      {selectedTeachers.map((teacher) => (
+                        <Chip
+                          key={teacher.id}
+                          className="chip"
+                          onDelete={() => handleRemoveTeacher(teacher.id)}
+                          onChange={handleChange("professorID")}
+                          color="success"
+                          avatar={
+                            <Avatar
+                              alt={teacher.name}
+                              src="/static/images/avatar/1.jpg"
+                            />
+                          }
+                          label={teacher.name}
+                        />
+                      ))}
+                    </Stack>
+                  </>
                 )}
-                {errors.professorID && touched.professorID && (
-                  <span className="error text">{errors.professorID}</span>
+                {errors.professors && touched.professors && (
+                  <span className="error text">{errors.professors}</span>
                 )}
                 <div className="buttons-container">
                   <button onClick={() => navigate(-1)} type="button">
