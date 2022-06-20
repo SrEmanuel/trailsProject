867f73ad70929806ea5c471689f92a27bf9317d7
--replace <edmarcosfilho2015@gmail.com>
2022-06-20 07:13:37 -0300

    update: added confirmation modal on click in delete account button and handleDeleteAccount function when it is confirmed

--- frontend/src/components/Profile/index.tsx
+++ frontend/src/components/Profile/index.tsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
 import { FiLock, FiTrash2, FiX } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { useAuth } from "../../hooks/useAuth";
+import api from "../../services/api";
+import { handleNotifyError } from "../../utils/handleNotifyError";
+import { ConfirmationModal } from "../ConfirmationModal";
 import { Editable } from "../Editable";
 import { ModalContainer } from "../ModalContainer";
 import { Overlay } from "../Overlay";
@@ -12,30 +18,69 @@ interface Props {
 }
 
 export function Profile({ isVisible, setIsVisible }: Props) {
-  const { user } = useAuth();
+  const [deleteAccount, setDeleteAccount] = useState<boolean>(false);
+  const { user, handleClearUserDataFromStorage } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleDeleteAccount() {
+    try {
+      await api.delete(`users/${user?.id}`);
+      toast.success("Conta removida com sucesso");
+      await handleClearUserDataFromStorage();
+      navigate('/')
+    } catch (error) {
+      handleNotifyError(error, navigate, handleClearUserDataFromStorage);
+    }
+  }
 
   return (
-    <Overlay hidden={!isVisible}>
-      <ModalContainer>
-        <FiX color="var(--red)" size={24} onClick={() => setIsVisible(false)} />
-        <span className="profile">{user?.name.substring(0, 1)}</span>
-        {user && (
-          <>
-            <Editable label="NOME:" value={user?.name} onSubmit={() => {}} />
-            <Editable type="email" label="E-MAIL:" value={user?.username} onSubmit={() => {}} />
-          </>
-        )}
-        <div className="row-wrapper bottom">
-        <button type="button" className="del-btn" >
-          <FiTrash2 color="var(--white)" size={20} />
-         <span> Excluir conta</span>
-        </button>
-        <button type="button">
-          <FiLock color="var(--white)" size={20} />
-         <span>Alterar senha</span>
-        </button>
-        </div>
-      </ModalContainer>
-    </Overlay>
+    <>
+      <Overlay hidden={!isVisible}>
+        <ModalContainer>
+          <FiX
+            color="var(--red)"
+            size={24}
+            onClick={() => setIsVisible(false)}
+          />
+          <span className="profile">{user?.name.substring(0, 1)}</span>
+          {user && (
+            <>
+              <Editable label="NOME:" value={user?.name} onSubmit={() => {}} />
+              <Editable
+                type="email"
+                label="E-MAIL:"
+                value={user?.username}
+                onSubmit={() => {}}
+              />
+            </>
+          )}
+          <div className="row-wrapper bottom">
+            <button
+              type="button"
+              className="del-btn"
+              onClick={() => {
+                setDeleteAccount(true);
+                setIsVisible(false);
+              }}
+            >
+              <FiTrash2 color="var(--white)" size={20} />
+              <span> Excluir conta</span>
+            </button>
+            <button type="button">
+              <FiLock color="var(--white)" size={20} />
+              <span>Alterar senha</span>
+            </button>
+          </div>
+        </ModalContainer>
+      </Overlay>
+      <ConfirmationModal
+        isVisible={deleteAccount}
+        setIsVisible={setDeleteAccount}
+        onConfirm={handleDeleteAccount}
+        message="Deseja mesmo deletar sua conta?"
+        confirmText="Sim, deletar"
+        cancelText="Não, cancelar"
+      />
+    </>
   );
 }
