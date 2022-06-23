b3856338885183c7e7a02696b519a5943e2d1d38
--replace <edmarcosfilho2015@gmail.com>
2022-06-23 11:24:17 -0300

    update: added mode prop to define when it's to create or update a topic | added currentCourseLinkName prop to store the course linkName to be used as identifier on the endpoint to reload the list of topics with the updated results | added linkName prop to be the initial value of the form if it is an update operation

--- frontend/src/components/AddOrUpdateSection/index.tsx
+++ frontend/src/components/AddOrUpdateSection/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { Formik } from "formik";
 import { FiX } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
@@ -15,7 +16,10 @@ import "./styles.scss";
 
 interface Props {
   isVisible: boolean;
-  currentCourse: ITrails;
+  currentCourse?: ITrails;
+  currentCourseLinkName?: string;
+  mode: "create" | "update";
+  linkName?: string;
   setIsVisible: (arg: boolean) => void;
   setTopics: React.Dispatch<React.SetStateAction<ITopic[] | undefined>>;
 }
@@ -23,46 +27,69 @@ interface Props {
 export function AddOrUpdateSection({
   isVisible,
   currentCourse,
+  currentCourseLinkName,
+  mode,
+  linkName,
   setIsVisible,
   setTopics,
 }: Props) {
   const { handleClearUserDataFromStorage } = useAuth();
   const navigate = useNavigate();
+  const formikRef = useRef() as any;
 
   async function handleSumitValuesAndClose(name: string) {
-    const topic = {
-      name: name,
-      position: 100,
-      courseId: currentCourse?.id,
-    };
     try {
-      await api.post("/topics", topic);
-      const response = await api.get(
-        `/courses/${currentCourse?.linkName}/topics`
-      );
-      setTopics(response.data.content);
+      if (mode === "create") {
+        const topic = {
+          name: name,
+          position: 100,
+          courseId: currentCourse?.id,
+        };
+        await api.post("/topics", topic);
+        const response = await api.get(
+          `/courses/${currentCourse?.linkName}/topics`
+        );
+        setTopics(response.data.content);
+      } else {
+        const topic = { name, position: 100 }
+        await api.put(`/topics/${linkName}`, topic);
+        const response = await api.get(
+          `/courses/${currentCourseLinkName}/topics`
+        );
+        setTopics(response.data.content);
+      }
     } catch (error) {
       handleNotifyError(error, navigate, handleClearUserDataFromStorage);
     }
     setIsVisible(false);
   }
 
+  useEffect(() => {
+    formikRef.current.setFieldValue("name", linkName);
+  }, [linkName]);
+
   return (
     <Overlay hidden={!isVisible}>
       <ModalContainer>
         <FiX size={24} color="var(--red)" onClick={() => setIsVisible(false)} />
         <Formik
+          innerRef={formikRef}
           initialValues={{ name: "" }}
           validationSchema={Yup.object().shape({
             name: Yup.string().required("O nome é obrigatório"),
           })}
           onSubmit={(values) => handleSumitValuesAndClose(values.name)}
         >
-          {({ handleChange, handleSubmit, errors, touched }) => (
+          {({ handleChange, handleSubmit, errors, touched, values }) => (
             <>
-              <h2>Deseja adicionar uma nova sessão?</h2>
+              <h2>
+                {mode === "create"
+                  ? "Deseja adicionar uma nova sessão?"
+                  : "Atualizar sessão"}
+              </h2>
               <input
                 name="name"
+                defaultValue={values.name}
                 onChange={handleChange}
                 placeholder="Nome da sessão"
               />
@@ -74,7 +101,7 @@ export function AddOrUpdateSection({
                   Cancelar
                 </button>
                 <button type="submit" onClick={handleSubmit as any}>
-                  Criar
+                  {mode === "create" ? "Criar" : "Atualizar"}
                 </button>
               </div>
             </>
