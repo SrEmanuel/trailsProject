94f7d9cc65b8cbe07e013c1406bed1ca7c60840b
--replace <edmarcosfilho2015@gmail.com>
2022-06-23 07:53:33 -0300

    refactor: moved AddNewSection into components folder and renamed to AddOrUpdatesection

--- frontend/src/components/AddOrUpdateSection/index.tsx
+++ frontend/src/components/AddOrUpdateSection/index.tsx
@@ -3,13 +3,13 @@ import { FiX } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 
-import { ModalContainer } from "../../../../components/ModalContainer";
-import { Overlay } from "../../../../components/Overlay";
-import { useAuth } from "../../../../hooks/useAuth";
-import { ITopic } from "../../../../interfaces/topic";
-import { ITrails } from "../../../../interfaces/Trail";
-import api from "../../../../services/api";
-import { handleNotifyError } from "../../../../utils/handleNotifyError";
+import { ModalContainer } from "../ModalContainer";
+import { Overlay } from "../Overlay";
+import { useAuth } from "../../hooks/useAuth";
+import { ITopic } from "../../interfaces/topic";
+import { ITrails } from "../../interfaces/Trail";
+import api from "../../services/api";
+import { handleNotifyError } from "../../utils/handleNotifyError";
 
 import "./styles.scss";
 
@@ -20,25 +20,30 @@ interface Props {
   setTopics: React.Dispatch<React.SetStateAction<ITopic[] | undefined>>;
 }
 
-export function AddNewSection({ isVisible, currentCourse , setIsVisible, setTopics }: Props) {
+export function AddOrUpdateSection({
+  isVisible,
+  currentCourse,
+  setIsVisible,
+  setTopics,
+}: Props) {
   const { handleClearUserDataFromStorage } = useAuth();
   const navigate = useNavigate();
 
-  async function handleSumitValuesAndClose(name: string){
-      const topic = {
-        name: name,
-        position: 100,
-        courseId: currentCourse?.id,
-      };
-      try {
-        await api.post("/topics", topic);
-        const response = await api.get(
-          `/courses/${currentCourse?.linkName}/topics`
-        );
-        setTopics(response.data.content);
-      } catch (error) {
-        handleNotifyError(error, navigate, handleClearUserDataFromStorage);
-      }
+  async function handleSumitValuesAndClose(name: string) {
+    const topic = {
+      name: name,
+      position: 100,
+      courseId: currentCourse?.id,
+    };
+    try {
+      await api.post("/topics", topic);
+      const response = await api.get(
+        `/courses/${currentCourse?.linkName}/topics`
+      );
+      setTopics(response.data.content);
+    } catch (error) {
+      handleNotifyError(error, navigate, handleClearUserDataFromStorage);
+    }
     setIsVisible(false);
   }
 
@@ -51,7 +56,7 @@ export function AddNewSection({ isVisible, currentCourse , setIsVisible, setTopi
           validationSchema={Yup.object().shape({
             name: Yup.string().required("O nome é obrigatório"),
           })}
-          onSubmit={(values) =>  handleSumitValuesAndClose(values.name) }
+          onSubmit={(values) => handleSumitValuesAndClose(values.name)}
         >
           {({ handleChange, handleSubmit, errors, touched }) => (
             <>
@@ -61,7 +66,9 @@ export function AddNewSection({ isVisible, currentCourse , setIsVisible, setTopi
                 onChange={handleChange}
                 placeholder="Nome da sessão"
               />
-              { errors.name && touched.name && <span className="error" >{errors.name}</span> }
+              {errors.name && touched.name && (
+                <span className="error">{errors.name}</span>
+              )}
               <div className="btn-container">
                 <button type="button" onClick={() => setIsVisible(false)}>
                   Cancelar
