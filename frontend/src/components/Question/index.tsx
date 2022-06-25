4a8b79c48522672dbec503b1524f70ceb0fce0fe
--replace <edmarcosfilho2015@gmail.com>
2022-06-25 11:05:18 -0300

    update: added handleNextQuestion prop to update the state and show the next question, added currentQuestion and totalQuestion to know if there is a next question, added isAnswered state to block the next button if there is no asnwer submited, added selectedOption to sendand compare to the correct answer

--- frontend/src/components/Question/index.tsx
+++ frontend/src/components/Question/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.scss";
 
 interface IAlternatives {
@@ -13,11 +14,30 @@ interface IQuestion {
 
 interface Props {
   data: IQuestion;
+  handleNextQuestion: (arg:number) => void;
+  currentQuestion: number;
+  totalQuestions: number;
 }
 
-export function Question({ data }: Props) {
+export function Question({ data, currentQuestion, totalQuestions, handleNextQuestion }: Props) {
+  const [isAnswered, setIsAnswered] = useState<boolean>(false);
+  const [selectedOption, setSelectedOption] = useState<string>('');
+
+  function nextQuestion(){
+    if(currentQuestion < totalQuestions){
+      handleNextQuestion(currentQuestion + 1)
+    }
+  }
+
+  function handleSubmitAnswer(){
+    setIsAnswered(true);
+    
+  }
+
   return (
     <div className="question-container">
+      <div className="line"></div>
+      <h2>Exercício</h2>
       <div
         className="html-content"
         dangerouslySetInnerHTML={{ __html: data?.htmlContent as string }}
@@ -25,14 +45,14 @@ export function Question({ data }: Props) {
       <div className="alternatives">
         {data.alternatives.map((alternative) => (
           <div key={alternative.id} className="alternative">
-            <input type="radio" id={alternative.id as unknown as string } name="alternative" />
+            <input onClick={ () => setSelectedOption(alternative.text) } type="radio" id={alternative.id as unknown as string } name="alternative" />
             <label htmlFor={alternative.id as unknown as string}>{alternative.text}</label>
           </div>
         ))}
       </div>
       <div className="controls">
         <button>Enviar Resposta</button>
-        <button>Próximo</button>
+        <button disabled={ !(currentQuestion < totalQuestions ) || !isAnswered } onClick={nextQuestion} >Próximo</button>
       </div>  
     </div>
   );
