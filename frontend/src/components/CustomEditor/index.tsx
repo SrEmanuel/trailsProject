92fc205633a6cc0a2970294903dd2c3ca3d2b376
--replace <edmarcosfilho2015@gmail.com>
2022-06-26 00:10:51 -0300

    create: CustomEditor component that is a wrapper for the TinyMCE editor

---
+++ frontend/src/components/CustomEditor/index.tsx
@@ -0,0 +1,31 @@
+import { Editor } from "@tinymce/tinymce-react";
+import { useRef } from "react";
+
+
+export function CustomEditor() {
+  const editorRef = useRef() as any;
+  return (
+    <div className="editor">
+      <Editor
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          height: 500,
+          menubar: true,
+          plugins: [
+            "advlist autolink lists link image imagetools charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media image table paste code help wordcount",
+          ],
+          toolbar:
+            "undo redo | formatselect | " +
+            "bold italic backcolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | image | media | help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px;}",
+        }}
+      />
+    </div>
+  );
+}
