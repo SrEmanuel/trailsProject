038d3d5f276d0a9a1bc3dc56e498199e19dc41c8
--replace <edmarcosfilho2015@gmail.com>
2022-06-26 17:16:38 -0300

    create: IQuestion interface

---
+++ frontend/src/interfaces/question.ts
@@ -0,0 +1,10 @@
+export interface IQuestion{
+  id: number;
+  htmlContent: string;
+  correct: string;
+  answerA: string;
+  answerB: String
+  answerC: String
+  answerD: String
+  answerE: String
+}
