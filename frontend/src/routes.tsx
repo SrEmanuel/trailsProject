0da05201478d112bcf9bd07f8c63ad64bd064658
--replace <edmarcosfilho2015@gmail.com>
2022-06-28 08:29:13 -0300

    fix: changed route name to include coursename and topicid params

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -26,8 +26,8 @@ export function Routes(){
              <Route path="/cursos/:coursename" element={<ListContent></ListContent>} />
              <Route path="/cursos/:courseid/:blogtitle" element={<BlogPost></BlogPost>} />
              <Route path="/novo-curso" element={<CreateCourse></CreateCourse>} />
-             <Route path="/cursos/adicionar" element={<PostForm></PostForm>} />
              <Route path="/cursos/atualizar/:coursename" element={<CreateCourse></CreateCourse>} />
+             <Route path="/cursos/:coursename/:topicid/adicionar" element={<PostForm></PostForm>} />
              <Route path="/cursos/:courseid/:topicId/novo-conteudo" element={<CreateContent></CreateContent>} />
              <Route path="/cursos/:coursename/:topicId/:contentname/atualizar" element={<CreateContent></CreateContent>} />
              <Route path="*" element={<PageNotFound></PageNotFound>} />
