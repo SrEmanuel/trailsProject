3fcccdc7a94d9d5f1cea507b482790bc1837f50c
--replace <edmarcosfilho2015@gmail.com>
2022-06-28 16:53:31 -0300

    refactor: removed QuestionSchema and added atributes in PostSchema

--- frontend/src/schemas/post.schema.ts
+++ frontend/src/schemas/post.schema.ts
@@ -1,19 +1,15 @@
 import * as Yup from "yup";
 
-const QuestionSchema = Yup.object().shape({
-  htmlContent: Yup.string().required('Um conteúdo é necessário'),
-  correct: Yup.string().required('É necessário marcar uma opção como correta'),
-  answerA: Yup.string().required('É necessario preencher essa alternativa'),
-  answerB: Yup.string().required('É necessario preencher essa alternativa'),
-  answerC: Yup.string().required('É necessario preencher essa alternativa'),
-  answerD: Yup.string().required('É necessario preencher essa alternativa'),
-  answerE: Yup.string().required('É necessario preencher essa alternativa'),
-})
-
 export const PostSchema = Yup.object().shape({
- name: Yup.string().required('O titulo é um campo obrigatório'),
- grade: Yup.string().required('A śerie é um campo obrigatório'),
- image:Yup.mixed().required('A imagem é um campo obrigatório'),
- htmlContent: Yup.string().required('Um conteúdo é necessário'),
- questions: Yup.array().of(QuestionSchema)
+  name: Yup.string().required("O titulo é um campo obrigatório"),
+  grade: Yup.string().required("A śerie é um campo obrigatório"),
+  image: Yup.mixed().required("A imagem é um campo obrigatório"),
+  htmlContent: Yup.string().required("Um conteúdo é necessário"),
+  QuestionHtmlContent: Yup.string().required("Um conteúdo é necessário"),
+  correct: Yup.string().required("É necessário marcar uma opção como correta"),
+  answerA: Yup.string().required("É necessario preencher essa alternativa"),
+  answerB: Yup.string().required("É necessario preencher essa alternativa"),
+  answerC: Yup.string().required("É necessario preencher essa alternativa"),
+  answerD: Yup.string().required("É necessario preencher essa alternativa"),
+  answerE: Yup.string().required("É necessario preencher essa alternativa"),
 });
